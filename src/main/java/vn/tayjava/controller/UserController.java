e0cf263dc668a973ec6dc11c5882bc01c5490eda
Tây Java <quoctay87@gmail.com>
2024-10-18 12:34:28 +0700

    update

--- src/main/java/vn/tayjava/controller/UserController.java
+++ src/main/java/vn/tayjava/controller/UserController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.tayjava.controller.request.ChangePwdRequest;
