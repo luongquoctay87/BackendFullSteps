6f2f72ed3970d7f7bd6a520c51516c6560a7c684
Lương Quốc Tây <luongquoctay87@gmail.com>
2024-10-23 12:14:54 +0700

    monitoring

--- src/test/java/vn/tayjava/BackendServiceApplicationTests.java
+++ src/test/java/vn/tayjava/BackendServiceApplicationTests.java
@@ -1,14 +1,9 @@
 package vn.tayjava;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-
 //@SpringBootTest
-//@Profile("test")
 class BackendServiceApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
